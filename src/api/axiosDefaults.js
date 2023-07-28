d1811384b2ffcc8679aa8b36f5b5679bda38c221
rpf13 <rpf13@me.com>
2023-07-28 09:30:24 +0000

    feat: Add axios interceptors for request, response
    This commit will add the axios interceptors to intercept the request
    and the responses to our api.
    The response interceptor listes to 401 error and if so, tries to
    refresh the access token
    The request interceptor will refresh the access token each time
    before we execute a new request to the api

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,9 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://sportsshooting-drf-rpf13-5060e23f8756.herokuapp.com/';
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+// intercept the request
+export const axiosReq = axios.create();
+
+// intercept the response
+export const axiosRes = axios.create();
