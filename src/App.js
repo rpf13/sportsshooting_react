43f4402c97ca709269849c820535048b28aa05c0
rpf13 <rpf13@me.com>
2023-07-29 16:04:11 +0000

    feat: Add the match create site (initial version)
    This commit will add an initial version of the match create site.
    It adds the form with all the fields as defined in my backend
    as well as the option to upload and change an image.
    
    Styling is not finished yet as well as the functionality. Not
    linked to the API yet.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
+import MatchCreateForm from "./pages/matches/MatchCreateForm";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/" render={() => <h1>Matches</h1>} />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route exact path="/matches/create" render={() => <MatchCreateForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
