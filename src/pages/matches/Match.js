5870e933ebb2bab1ebeb59e5e56e6683915d7dac
rpf13 <rpf13@me.com>
2023-08-01 07:25:04 +0000

    feat: Add context to match page
    This commit will add further context to the Match page. It will include
    the rest of the missing fields from the DB.
    It will also add the Attending icon as well as the comments icon.
    
    The Attending functionality is wired up so that a logged in user,
    including the owner of a match post, can attend a match by clicking
    on the icon. The icon will be persistend tred and the count will
    increase by one.
    The unattend logic is just the reverse logic of the above.
    If a user is not logged in and wants to attend a match, the icon will
    turn black and an overlay text gets displayed to log in first.

--- src/pages/matches/Match.js
+++ src/pages/matches/Match.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from "../../styles/Match.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
-import { Card, Media } from 'react-bootstrap';
+import { Card, Media, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { Link, useHistory } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import FormatDate from '../../helper/FormatDate';
+import { axiosRes } from "../../api/axiosDefaults";
 
 const Match = (props) => {
     const {
@@ -24,10 +25,45 @@ const Match = (props) => {
         details,
         updated_at,
         matchPage,
+        setMatches,
     } = props;
 
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner
+
+    // function to handle the attending logic
+    const handleAttend = async () => {
+      try {
+        const {data} = await axiosRes.post("/attendings/", { match: id });
+        setMatches((prevMatches) => ({
+          ...prevMatches,
+          results: prevMatches.results.map((match) => {
+            return match.id === id
+            ? {...match, attendings_count: match.attendings_count + 1, attending_id: data.id}
+            : match;
+          })
+        }))
+      } catch (err) {
+        console.log(err)
+      }
+    };
+
+    // function to handle the unattend logic
+    const handleUnattend = async () => {
+      try {
+        await axiosRes.delete(`/attendings/${attending_id}`);
+        setMatches((prevMatches) => ({
+          ...prevMatches,
+          results: prevMatches.results.map((match) => {
+            return match.id === id
+            ? {...match, attendings_count: match.attendings_count -1, attending_id: null}
+            : match;
+          }),
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
 
   return <Card className={styles.Match}>
@@ -52,6 +88,32 @@ const Match = (props) => {
       {title && <Card.Title className='text-center'>{title} - <FormatDate match_date={match_date} /></Card.Title>}
       {title && <Card.Text className='text-center'>{match_location}</Card.Text>}
       {title && <Card.Text className='text-center'>{level_filter}</Card.Text>}
+      {title && <Card.Text className='text-center'>{division}</Card.Text>}
+      {title && <Card.Text className='text-center'>{details}</Card.Text>}
+      <div className={styles.MatchBar}>
+        {/* logged in user incl. owner must be able to attend a match */}
+        {attending_id ? (
+          <span onClick={handleUnattend}>
+            <i className={`fa-regular fa-calendar-check ${styles.Attend}`} />
+          </span>
+        ) : currentUser ? (
+          <span onClick={handleAttend}>
+            <i className={`fa-regular fa-calendar-check ${styles.AttendOutline}`} />
+          </span>
+        ) : (
+          <OverlayTrigger
+            placement="top"
+            overlay={<Tooltip>Log in to attend a Match!</Tooltip>}
+          >
+            <i className={`fa-regular fa-calendar-check ${styles.AttendUnauth}`} />
+          </OverlayTrigger>
+        )}
+        {attendings_count}
+        <Link to={`/matches/${id}`}>
+          <i className="fa-regular fa-comments" />
+        </Link>
+        {comments_count}
+      </div>
     </Card.Body>
   </Card>
 }
