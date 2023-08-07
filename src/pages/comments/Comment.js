65338388d4e8b800e4d73bcd8a823672cccc2fbf
rpf13 <rpf13@me.com>
2023-08-07 16:01:01 +0000

    feat: Add edit comment functionality
    This commit will add the edit comment functionality to the project.
    It's let's the logged in user, which is also the owner of the
    comment, edit his comment. There is the option to cancel the edit
    form.
    Styling has been updated to reflect the app's overall styling.

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from "react";
 import styles from '../../styles/Comment.module.css'
 import { Media } from 'react-bootstrap'
 import Avatar from '../../components/Avatar'
@@ -6,66 +6,80 @@ import { Link } from "react-router-dom";
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import { MoreDropdown } from '../../components/MoreDropdown';
 import { axiosRes } from '../../api/axiosDefaults';
+import CommentEditForm from "./CommentEditForm";
 
 const Comment = (props) => {
-    const {
-        profile_id,
-        profile_image,
-        owner,
-        updated_at,
-        content,
-        id,
-        setMatch,
-        setComments,
-    } = props
+  const {
+    profile_id,
+    profile_image,
+    owner,
+    updated_at,
+    content,
+    id,
+    setMatch,
+    setComments,
+  } = props;
 
-    const currentUser = useCurrentUser();
-    // check if currentUser is the owner of the comment
-    const is_owner = currentUser?.username === owner;
+  const [showEditForm, setShowEditForm] = useState(false);
+  const currentUser = useCurrentUser();
+  // check if currentUser is the owner of the comment
+  const is_owner = currentUser?.username === owner;
 
-    const handleDelete = async () => {
-        try {
-            await axiosRes.delete(`/comments/${id}/`)
-            setMatch((prevMatch) => ({
-                results: [
-                    {
-                    ...prevMatch.results[0],
-                    comments_count: prevMatch.results[0].comments_count -1,
-                },
-            ],
-            }));
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/comments/${id}/`);
+      setMatch((prevMatch) => ({
+        results: [
+          {
+            ...prevMatch.results[0],
+            comments_count: prevMatch.results[0].comments_count - 1,
+          },
+        ],
+      }));
 
-            setComments((prevComments) => ({
-                ...prevComments,
-                results: prevComments.results.filter((comment) => comment.id !== id),
-            }));
-        } catch (err) {
-            console.log(err)
-        }
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.filter((comment) => comment.id !== id),
+      }));
+    } catch (err) {
+      console.log(err);
     }
-
+  };
 
   return (
     <div>
-        <hr />
-        <Media>
-            <Link to={`/profiles/${profile_id}`}>
-                <Avatar src={profile_image} />
-            </Link>
-            <Media.Body className='allign-self-center ml-2'>
-                <Link to={`/profiles/${profile_id}`}>
-                    <span className={styles.Owner}>{owner}</span>
-                </Link>
-                <span className={styles.Date}>{updated_at}</span>
-                <p>{content}</p>
-            </Media.Body>
-            {is_owner && (
-                <MoreDropdown handleEdit={() => {}} handleDelete={handleDelete} />
+      <hr />
+      <Media>
+        <Link to={`/profiles/${profile_id}`}>
+          <Avatar src={profile_image} />
+        </Link>
+        <Media.Body className="allign-self-center ml-2">
+          <Link to={`/profiles/${profile_id}`}>
+            <span className={styles.Owner}>{owner}</span>
+          </Link>
+          <span className={styles.Date}>{updated_at}</span>
+          {showEditForm ? (
+            <CommentEditForm
+                id={id}
+                profile_id={profile_id}
+                content={content}
+                profileImage={profile_image}
+                setComments={setComments}
+                setShowEditForm={setShowEditForm}
+            /> 
+            ) : (
+            <p>{content}</p>
             )}
-        </Media>
-
+        </Media.Body>
+        {is_owner && !showEditForm && (
+          <MoreDropdown
+            handleEdit={() => setShowEditForm(true)}
+            handleDelete={handleDelete}
+          />
+        )}
+      </Media>
     </div>
-  )
-}
+  );
+};
 
-export default Comment
+export default Comment;
