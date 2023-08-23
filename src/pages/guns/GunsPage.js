e0f35a9a0da75db84cec70e34ab3b78986798315
rpf13 <rpf13@me.com>
2023-08-23 09:00:17 +0000

    refactor: Add updated version of GunsPage
    This commit will refactor the GunsPage. It will remove the option
    to display an error of not being logged in, if an unauthenticated user
    tries to access the url without being logged in. I somehow do not like
    it, therefore I have implemented that the user gets pushed to the main
    page, which is accessible for everyone.

--- src/pages/guns/GunsPage.js
+++ src/pages/guns/GunsPage.js
@@ -1,18 +1,15 @@
 import React, { useEffect, useState } from "react";
-
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import FormControl from "react-bootstrap/FormControl";
-
-import Gun from "./Gun"
+import Gun from "./Gun";
 import appStyles from "../../App.module.css";
 import styles from "../../styles/GunsPage.module.css";
-import { useLocation } from "react-router";
+import { useLocation, useHistory } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
-
-import NoResults from "../../assets/no_results.png"
+import NoResults from "../../assets/no_results.png";
 import Asset from "../../components/Asset";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import InfiniteScroll from "react-infinite-scroll-component";
@@ -20,27 +17,25 @@ import { fetchMoreData } from "../../helper/utils";
 import PopularMatches from "../matches/PopularMatches";
 import { useRedirect } from "../../hooks/useRedirect";
 
-
 function GunsPage({ message }) {
-  useRedirect('loggedOut')
+  const history = useHistory();
   const [guns, setGuns] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
-  const [hasError, setHasError] = useState(false);
   const { pathname } = useLocation();
   const currentUser = useCurrentUser();
 
-  // used for search query
   const [query, setQuery] = useState("");
   const [gunType, setGunType] = useState("");
 
   useEffect(() => {
     const fetchGuns = async () => {
       try {
-        const { data } = await axiosReq.get(`/guns?search=${query}&type=${gunType}`);
+        const { data } = await axiosReq.get(
+          `/guns?search=${query}&type=${gunType}`
+        );
         setGuns(data);
         setHasLoaded(true);
       } catch {
-        setHasError(true);
         setHasLoaded(true);
       }
     };
@@ -55,13 +50,13 @@ function GunsPage({ message }) {
         clearTimeout(timer);
       };
     } else {
-      // If no user is logged in, set 'hasLoaded' and 'hasError' to true
-      // which will cause re-render and display the not authorized error
+      // If no user is logged in, set 'hasLoaded' to true
+      // and push to main route
       setHasLoaded(true);
-      setHasError(true);
-      setGuns({ results: [] }); // clear the guns state
+      setGuns({ results: [] });
+      history.push("/");
     }
-  }, [pathname, query, currentUser, gunType]);
+  }, [pathname, query, currentUser, gunType, history]);
 
   return (
     <Row className="h-100">
@@ -79,10 +74,8 @@ function GunsPage({ message }) {
             className="mr-sm-2"
             placeholder="Search by brand, model, serialnumber"
           />
-
           <Form.Control
             as="select"
-            placeholder="Choose GunType"
             value={gunType}
             onChange={(event) => setGunType(event.target.value)}
             className="mb-3"
@@ -93,32 +86,20 @@ function GunsPage({ message }) {
           </Form.Control>
         </Form>
         {hasLoaded ? (
-          hasError ? (
-            <Container className={appStyles.Content}>
-              <p>
-                You are not authorized to view this content.
-                <br />
-                Create an account and login first.
-              </p>
-            </Container>
+          guns.results.length ? (
+            <InfiniteScroll
+              children={guns.results.map((gun) => (
+                <Gun key={gun.id} {...gun} setGuns={setGuns} />
+              ))}
+              dataLength={guns.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!guns.next}
+              next={() => fetchMoreData(guns, setGuns)}
+            />
           ) : (
-            <>
-              {guns.results.length ? (
-                <InfiniteScroll
-                  children={guns.results.map((gun) => {
-                    return <Gun key={gun.id} {...gun} setGuns={setGuns} />;
-                  })}
-                  dataLength={guns.results.length}
-                  loader={<Asset spinner />}
-                  hasMore={!!guns.next}
-                  next={() => fetchMoreData(guns, setGuns)}
-                />
-              ) : (
-                <Container className={appStyles.Content}>
-                  <Asset src={NoResults} message={message} />
-                </Container>
-              )}
-            </>
+            <Container className={appStyles.Content}>
+              <Asset src={NoResults} message={message} />
+            </Container>
           )
         ) : (
           <Container className={appStyles.Content}>
